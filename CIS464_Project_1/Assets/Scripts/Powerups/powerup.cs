105aea45d76233046eaf5b79255a66a74f803c0a
cletendre3 <cletendre@umassd.edu>
2023-04-10 15:53:28 -0400

    implemented first stage of powerups

---
+++ CIS464_Project_1/Assets/Scripts/Powerups/powerup.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerup : MonoBehaviour
+{
+
+    public float multiplier = 1.2f;
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter (Collider other) {
+
+        if(other.CompareTag("Player")) {
+            Pickup(other);
+        }
+
+    }
+
+    void Pickup(Collider player) {
+
+        //spawn effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //apply effect to player
+        player.transform.localScale *= multiplier;
+
+        
+        //delete object
+        Destroy(gameObject);
+    }
+}
